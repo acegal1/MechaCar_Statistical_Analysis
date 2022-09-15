1b12f8dc36dc770cb2a338e2a92c6ebda7bcfc0b
Connie Aceves <acevesc@gmail.com>
2022-09-15 14:49:49 -0700

    Added Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,29 @@
 #Deliverable 1: Linear Regression to Predict MP
+#Using your knowledge of R, you’ll design a linear model that predicts the mpg of MechaCar prototypes
+#using several variables from the MechaCar_mpg.csv file.
+
 #save working directory for R Challenge
 setwd("C:/Users/acega/asu/hw/Mod_15_R/MechaCar_Statistical_Analysis")
 
 library(dplyr)
+library(tidyverse)
+#import and read as dataframe
 MechaCar_mpg <-read.csv('./data/MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+view(MechaCar_mpg)
+
+#•	Generate Multiple linear regression model with independent variables
+lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
+
+#Generate statistical metrics using the summary()function
+summary(lm(mpg~vehicle_length + vehicle_weight +
+             spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
+
+#Deliverable 2
+#import and read as table 
+Suspension_Coil <- read.csv('./data/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#stat summary with summarize()
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+# stat summary wtih group_by()
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep')
